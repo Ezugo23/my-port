6d09a5d3db6e11076869055fb3bdf113f46fa036
iwu paschal <iwuchukwuezugo@gmail.com>
2023-08-21 22:39:21 +0100

    Initial commit

---
+++ portfolio.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+      if(this.scrollY > 20){
+        $('.navbar').addClass("sticky");
+      } else{
+        $('.navbar').removeClass("sticky");
+      }
+      if(this.scrollY > 500){
+        $('.scroll-up-btn').addClass("show");
+      } else{
+        $('.scroll-up-btn').removeClass("show");
+      }
+    });
+    //slide
+    $('.scroll-up-btn').click(function(){
+      $('html').animate({scrollTop: 0});
+    });
+
+    //togle menu/navbar
+    $('.menu-btn').click(function(){
+      $('.navbar .menu').toggleClass("active");
+      $('.menu-btn img').toggleClass("active");
+    });
+});
+//typing Animation
+var typed = new Typed(".typing", {
+  strings: ["Web Developer", "FrontEnd Dev", "FreeLancer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
+var typed = new Typed(".typing-2", {
+  strings: ["HTML", "CSS", "JavaScript", "React Js", "Git", "GitHub"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
